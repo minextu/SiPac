9dc75ae98d88a811bef297f3b844d4708cff3579
Kim Westesen <djraffid@web.de>
2013-07-24 19:56:15 +0200

    new theme wip

--- themes/default/layout.php
+++ themes/default/layout.php
@@ -10,8 +10,8 @@ $chat_layout = "
     <div class='chat_user_area'>
       <div class='chat_extra_bar'></div><!-- end: chat_extra_bar-class -->
       <div class='chat_user_input'>
-	<input type='text' class='chat_message'>
-	<button class='chat_send_button'></button><!-- end: chat_send_button-class -->
+	<input type='text' class='chat_message' placeholder='<||t34||>'>
+	<button class='chat_send_button'><||t12||></button><!-- end: chat_send_button-class -->
       </div><!-- end: chat_user_input-class -->
     </div><!-- end: chat_user_area-class -->
   </div><!-- end: chat_right-class -->
