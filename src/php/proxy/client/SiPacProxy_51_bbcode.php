260de4940aa0b0a2c45cf5667b46ee4d4f91f419
Jan Houben <next-play@web.de>
2014-07-30 00:40:21 +0200

    0.0.4-git (alpha9) fix1

--- src/php/proxy/client/SiPacProxy_51_bbcode.php
+++ src/php/proxy/client/SiPacProxy_51_bbcode.php
@@ -14,7 +14,7 @@ class SiPacProxy_bbcode implements SiPacProxy
 		$this->post['message'] = preg_replace('=\[u\](.*)\[/u\]=Uis','<span style="font-weight:underline;">$1</span>', $this->post['message']);
 		$this->post['message'] = preg_replace('=\[i\](.*)\[/i\]=Uis','<span style="font-weight:italic;">$1</span>', $this->post['message']);
 		$this->post['message'] = preg_replace('=\[s\](.*)\[/s\]=Uis','<span style="text-decoration:line-through;">$1</span>', $this->post['message']);
-		$this->post['message'] = preg_replace('=\[img\](.*)\[/img\]=Uis','<img src="$1" alt="$1"></img>', $this->post['message']);
+		$this->post['message'] = preg_replace('=\[img\](.*)\[/img\]=Uis','<img src="$1" alt="$1" style="max-width: 100%;  max-height: 300px;"></img>', $this->post['message']);
 		$this->post['message'] = preg_replace('#\[color=(.*)\](.*)\[/color\]#isU','<span style="color: $1">$2</span>', $this->post['message']);
 		return $this->post;
 	}
