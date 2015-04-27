1ca6d4e594311d8b9a274e4e3571584faaaf40d8
Jan Houben <next-play@web.de>
2015-04-27 16:42:07 +0200

    fixed underline and italic bbcode

--- src/php/proxy/client/SiPacProxy_51_bbcode.php
+++ src/php/proxy/client/SiPacProxy_51_bbcode.php
@@ -5,8 +5,8 @@ class SiPacProxy_bbcode extends SiPacProxy
 	public function execute()
 	{  
 		$this->post['message'] = preg_replace('-\[b\](.*?)\[/b\]-','<span style="font-weight:bold;">$1</span>', $this->post['message']);
-		$this->post['message'] = preg_replace('-\[u\](.*?)\[/u\]-','<span style="font-weight:underline;">$1</span>', $this->post['message']);
-		$this->post['message'] = preg_replace('-\[i\](.*?)\[/i\]-','<span style="font-weight:italic;">$1</span>', $this->post['message']);
+		$this->post['message'] = preg_replace('-\[u\](.*?)\[/u\]-','<span style="text-decoration:underline;">$1</span>', $this->post['message']);
+		$this->post['message'] = preg_replace('-\[i\](.*?)\[/i\]-','<span style="font-style: italic;">$1</span>', $this->post['message']);
 		$this->post['message'] = preg_replace('-\[s\](.*?)\[/s\]-','<span style="text-decoration:line-through;">$1</span>', $this->post['message']);
 		$this->post['message'] = preg_replace('-\[img\](https?://.*?\.(?:jpg|jpeg|png|gif|bmp|tif|svg))\[/img\]-i','<img src="$1" alt="$1" style="max-width: 100%;  max-height: 300px;"></img>', $this->post['message']);
 		$this->post['message'] = preg_replace('-\[color=([^;]*?)\](.*?)\[/color\]-','<span style="color: $1;">$2</span>', $this->post['message']);
